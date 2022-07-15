828cc2baf0841b501a41705bc07acf23556881bd
tuantt <tuantt@tohsoft.com>
2022-07-15 15:02:45 +0700

    add restAPI

---
+++ app/Http/Resources/StoryResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'slug' => $this->slug,
+            'name' => $this->name,
+            'image' => $this->image,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use App\Http\Resources\StoryResource;
+use App\Models\Story;
+ 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +21,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/stories', function () {
+    return StoryResource::collection(Story::all());
+});
+
+Route::get('/categories', function () {
+    return CategoryResource::collection(Category::all());
 });
